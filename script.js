36f51c81e634c41db765dfb0098f186f54b88613
ArdaYesil <250542013@firat.edu.tr>
2026-03-27 14:23:11 +0300

    düzeltme

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function copyEmail() {
 }
 
 
+const apiKey = "66E34E3FCE0A2BCD77AB23C16F9F39BC";
+const steamID = "76561199184319290";
+
+fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamID}&include_appinfo=true`)
+.then(res => res.json())
+.then(data => {
+
+  const games = data.response.games;
+
+  const cs = games.find(game => game.appid === 730);
+
+  if(cs){
+    const hours = (cs.playtime_forever / 60).toFixed(1);
+    document.getElementById("cs-hours").innerText =
+      "CS2 Oynama Süresi: " + hours + " saat";
+  } else {
+    document.getElementById("cs-hours").innerText =
+      "CS bulunamadı";
+  }
+
+});
+
+
+
