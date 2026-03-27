061cb19f2d72f9289e53b1796a8d234f28790e1a
ArdaYesil <250542013@firat.edu.tr>
2026-03-27 11:27:09 +0300

    button

--- script.js
+++ script.js
@@ -3,30 +3,12 @@ window.onblur=() =>
 document.title="Geri Gel :(";
 window.onfocus= () =>
 document.title=baslik;
-// Butonu ve mesaj elementini seçiyoruz
-const buton = document.getElementById('kopyalaButon');
-const mesaj = document.getElementById('mesaj');
 
-// Kopyalanmasını istediğin Gmail adresin
-const gmailAdresi = "seninadın@gmail.com";
-
-buton.addEventListener('click', () => {
-    // Kopyalama işlemini başlatan komut
-    navigator.clipboard.writeText(gmailAdresi).then(() => {
-        // İşlem başarılıysa burası çalışır
-        mesaj.style.display = 'inline';
-        
-        // 2 saniye sonra "Kopyalandı" yazısını geri gizleyelim
-        setTimeout(() => {
-            mesaj.style.display = 'none';
-        }, 2000);
-        
-        console.log("E-posta başarıyla kopyalandı!");
-    }).catch(err => {
-        // Bir hata oluşursa burası çalışır
-        console.error("Kopyalama başarısız oldu: ", err);
-    });
-});
-
+function copyEmail() {
+  const email = "seninmail@gmail.com";
+  navigator.clipboard.writeText(email).then(() => {
+    alert("Email kopyalandı!");
+  });
+}
 
 
