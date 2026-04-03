b3f6c1b3569e90768687446e697b06c69fce7d83
ArdaYesil <250542013@firat.edu.tr>
2026-04-03 14:26:07 +0300

    mail deneme

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ window.onblur = () =>
 window.onfocus = () =>
   document.title = baslik;
 
+function copyEmail() {
+  const email = "[Yesilarda83@gmail.com]";
+  navigator.clipboard.writeText(email).then(() => {
+    alert("Email kopyalandı!");
+  });
+}
+
 // Tema değiştirme (Dark/Light mode)
 function toggleTheme() {
   document.body.classList.toggle('dark-mode');
