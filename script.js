71ac076401491b83c1c389b95eeb968bc4805be7
ArdaYesil <250542013@firat.edu.tr>
2026-04-03 14:17:55 +0300

    Mode Güncelleme

--- script.js
+++ script.js
@@ -4,26 +4,19 @@ window.onblur = () =>
 window.onfocus = () =>
   document.title = baslik;
 
-function copyEmail() {
-  const email = "yesilarda83@gmail.com";
-  navigator.clipboard.writeText(email).then(() => {
-    alert("Email kopyalandı!");
-  });
-}
-
 // Tema değiştirme (Dark/Light mode)
 function toggleTheme() {
   document.body.classList.toggle('dark-mode');
   const icon = document.getElementById("theme-icon");
   if (document.body.classList.contains('dark-mode')) {
-    // Karanlık moddaysak, ay simgesini göster
-    icon.classList.remove('fa-sun');
-    icon.classList.add('fa-moon');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    // Aydınlık moddaysak, güneş simgesini göster
+    // Karanlık moddaysak, güneş simgesini göster
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    // Aydınlık moddaysak, ay simgesini göster
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
     localStorage.setItem('theme', 'light');
   }
 }
@@ -35,8 +28,8 @@ window.addEventListener('DOMContentLoaded', () => {
   if (savedTheme === 'dark') {
     document.body.classList.add('dark-mode');
     if (icon) {
-      icon.classList.remove('fa-sun');
-      icon.classList.add('fa-moon');
+      icon.classList.remove('fa-moon');
+      icon.classList.add('fa-sun');
     }
   }
 });
